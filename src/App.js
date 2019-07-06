1dd95b4f3e6c91b311594ab1ff32cf239fd52a29
Yassine Mellouki <yassinemellouki4@gmail.com>
2019-07-05 18:17:02 -0700

    setup required packages & scripts

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="welcome">
+        <code>React Here!</code>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {minimize: true},
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {loader: MiniCssExtractPlugin.loader},
+          {loader: 'css-loader'},
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  devServer: {
+    host: '0.0.0.0',
+    open: true,
+  },
+};
